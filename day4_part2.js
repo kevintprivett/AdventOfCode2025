6f0ee1593656a8bfafc683d346887572eb850c5d
kevintprivett <kevintprivett@gmail.com>
2025-12-04 18:00:05 +0000

    Finish Day 4 Part 2

---
+++ day4_part2.js
@@ -0,0 +1,71 @@
+const fileInput = document.getElementById('problem_input')
+const solveButton = document.getElementById('solveButton')
+const resultText = document.getElementById('result')
+
+let inputArray = []
+
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0]
+
+  if (file) {
+    const reader = new FileReader()
+
+    reader.onload = (e) => {
+      const fileContent = e.target.result
+      inputArray = fileContent.trim().split('\n')
+
+      for (let i = 0; i < inputArray.length; i++) {
+        inputArray[i] = inputArray[i].split('')
+      }
+    }
+
+    reader.readAsText(file)
+  }
+})
+
+solveButton.addEventListener('click', () => {
+  let result = 0
+
+  const NEIGHBORS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ]
+
+  let changed = true
+
+  while (changed) {
+    changed = false
+
+    for (let i = 0; i < inputArray.length; i++) {
+      for (let j = 0; j < inputArray[i].length; j++) {
+        if (inputArray[i][j] !== '.') {
+          let neighborCount = 0
+
+          for (const [di, dj] of NEIGHBORS) {
+            // js stuff...
+            const neighbor = inputArray?.[i+di]?.[j+dj]
+
+            if (neighbor !== undefined && neighbor === '@') {
+              neighborCount++
+            }
+          }
+
+          if (neighborCount < 4) {
+            result++
+            inputArray[i][j] = '.'
+            changed = true
+          }
+        }
+      }
+    }
+  }
+
+  resultText.hidden = false;
+  resultText.innerText = `Result: ${result}`
+})
