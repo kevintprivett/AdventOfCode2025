2831a96b8f0096e4f102b78b1a2c030640771ccd
kevintprivett <kevintprivett@gmail.com>
2025-12-08 17:56:04 +0000

    Finish Day 8 Part 2.  Minor typo fixes for Part 1.

---
+++ day8_part2.js
@@ -0,0 +1,130 @@
+const fileInput = document.getElementById('problem_input')
+const solveButton = document.getElementById('solveButton')
+const resultText = document.getElementById('result')
+const timeText = document.getElementById('time')
+
+let inputArray = []
+
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0]
+
+  if (file) {
+    const reader = new FileReader()
+
+    reader.onload = (e) => {
+      const fileContent = e.target.result
+      inputArray = fileContent.trim().split('\n')
+
+      for (let i = 0; i < inputArray.length; i++) {
+        inputArray[i] = inputArray[i].split(',')
+
+        for (let j = 0; j < inputArray[i].length; j++) {
+          inputArray[i][j] = parseInt(inputArray[i][j])
+        }
+      }
+    }
+
+    reader.readAsText(file)
+  }
+})
+
+solveButton.addEventListener('click', () => {
+  const startTime = Date.now()
+
+  // helper: 3D euclidean distance
+  const euclideanDistance = (a, b) => {
+    const [ax, ay, az] = a
+    const [bx, by, bz] = b
+
+    return Math.sqrt(
+      (ax - bx) ** 2 +
+      (ay - by) ** 2 +
+      (az - bz) ** 2
+    )
+  }
+
+  // generate list of paths and distances
+  const allPaths = []
+
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    for (let j = i + 1; j < inputArray.length; j++) {
+      allPaths.push(
+        {
+          'path': `${inputArray[i].join(',')}-${inputArray[j].join(',')}`,
+          'distance': euclideanDistance(inputArray[i], inputArray[j])
+        }
+      )
+    }
+  }
+
+  // sort by distance (would be faster with fixed length heap, but js things)
+  allPaths.sort((a, b) => a.distance - b.distance)
+
+  // union-find
+  const unionFind = {}
+
+  for (let i = 0; i < inputArray.length; i++) {
+    const node = `${inputArray[i].join(',')}`
+    unionFind[node] = {
+      parent: node,
+      rank: 1
+    }
+  }
+
+  let maxSize = 1
+  let result = -1
+
+  // halt when all nodes have been connected
+  for (let i = 0; maxSize < inputArray.length ; i++) {
+    const curPath = allPaths[i].path
+
+    const [nodeAName, nodeBName] = curPath.split('-')
+
+    const nodeA = unionFind[nodeAName]
+    const nodeB = unionFind[nodeBName]
+
+    let parentA = nodeA
+    let parentAName = nodeAName
+    while (parentA.parent !== parentAName) {
+      parentAName = parentA.parent
+      parentA = unionFind[parentAName]
+    }
+
+    let parentB = nodeB
+    let parentBName = nodeBName
+    while (parentB.parent !== parentBName) {
+      parentBName = parentB.parent
+      parentB = unionFind[parentBName]
+    }
+
+    if (parentAName === parentBName) {
+      continue
+    }
+
+    let newParent = parentA
+    let newChild = parentB
+
+    if (parentB.rank > parentA.rank) {
+      newParent = parentB
+      newChild = parentA
+    }
+
+    newParent.rank = parentA.rank + parentB.rank
+    newChild.rank = 0
+
+    newChild.parent = newParent.parent
+
+    maxSize = Math.max(maxSize, newParent.rank)
+
+    if (maxSize === inputArray.length) {
+      const nodeAx = parseInt(nodeAName.split(',')[0])
+      const nodeBx = parseInt(nodeBName.split(',')[0])
+      result = nodeAx * nodeBx
+    }
+  }
+
+  resultText.hidden = false;
+  resultText.innerText = `Result: ${result}`
+  timeText.hidden = false;
+  timeText.innerText = `Time(ms): ${Date.now() - startTime}`
+})
